28aa82e7c97893a14073de86301caa919a49a330
Dan Faulkner <danfaulkner@hollandandbarrett.com>
2020-09-14 14:22:44 +0100

    fix: add table for stats

--- src/components/teams/team.js
+++ src/components/teams/team.js
@@ -1,41 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import datasheet from './datasheet'
-import Unit from '../unit/unit'
+import React, { useState, useEffect } from "react";
+import datasheet from "./datasheet";
+import Unit from "../unit/unit";
 
-const data = datasheet.team
+const data = datasheet.team;
 
 let team = true;
 let getUnit = false;
 
 const Team = () => {
-const [selectedTeam, updateTeam] = useState('')
+  const [selectedTeam, updateTeam] = useState("");
 
+  useEffect(() => {
+    getUnit = data.filter((teams) => teams.name === selectedTeam);
+    if (selectedTeam) {
+      console.log(getUnit[0].units);
+    }
+  }, [selectedTeam]);
 
-useEffect(() => {
-getUnit = data.filter(teams => teams.name === selectedTeam)
-if (selectedTeam) {
-console.log(getUnit[0].units)
-}
-    
-}, [selectedTeam])
-
-const handleChange = (e) => {
-    updateTeam(e.target.value)       
-}
+  const handleChange = (e) => {
+    updateTeam(e.target.value);
+  };
 
   return (
     <div className="App">
-     <label>Select a team</label>
-     <select value="select a team" onChange={handleChange} className="form-control">
-    <option value="">Select a team...</option>
-    {data.map(item => <option value={item.name} key={item.id}>{item.name}</option>)}
-     </select>
-     {team && (
-         <div className="teamBlock col-md-12"><p>You have selected: {selectedTeam}</p></div>
-     )}   
-     <Unit selectedTeam={selectedTeam} />  
+      <label>Select a team</label>
+      <select
+        value="select a team"
+        onChange={handleChange}
+        className="form-control"
+      >
+        <option value="">Select a team...</option>
+        {data.map((item) => (
+          <option value={item.name} key={item.id}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+      {team && (
+        <div className="teamBlock col-md-12">
+          <p>You have selected: {selectedTeam}</p>
+        </div>
+      )}
+      <Unit selectedTeam={selectedTeam} />
     </div>
   );
-}
+};
 
 export default Team;
--- src/components/unit/unit.js
+++ src/components/unit/unit.js
@@ -1,20 +1,57 @@
-import React from 'react'
-import datasheet from '../teams/datasheet'
+import React from "react";
+import datasheet from "../teams/datasheet";
 
 const Unit = (props) => {
+  const team = datasheet.team;
 
-   const team = datasheet.team
+  const filteredTeams = team.filter((item) =>
+    item.name.includes(props.selectedTeam)
+  );
+  const myUnits = filteredTeams[0].units;
+  console.log("chosen team", myUnits);
 
-   const filteredTeams = team.filter(item => item.name.includes(props.selectedTeam))
-   const myUnits = filteredTeams[0].units
-   console.log('chosen team', myUnits)
-
-   return (
-   <div>
-      {myUnits.map(unit => (
-         <div className="teamBlock"><h2>{unit.name} <span>Power rating: {unit.power}</span></h2></div>
+  return (
+    <div>
+      {myUnits.map((unit) => (
+        <>
+          <div className="teamBlock">
+            <h2>
+              {unit.name} <span>Power rating: {unit.power}</span>
+            </h2>
+          </div>
+          <div class="table-responsive-sm">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">M</th>
+                  <th scope="col">WS</th>
+                  <th scope="col">BS</th>
+                  <th scope="col">S</th>
+                  <th scope="col">T</th>
+                  <th scope="col">W</th>
+                  <th scope="col">A</th>
+                  <th scope="col">Ld</th>
+                  <th scope="col">Sv</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="col">{unit.M}"</th>
+                  <th scope="col">{unit.WS}+</th>
+                  <th scope="col">{unit.BS}+</th>
+                  <th scope="col">{unit.S}</th>
+                  <th scope="col">{unit.T}</th>
+                  <th scope="col">{unit.W}</th>
+                  <th scope="col">{unit.A}</th>
+                  <th scope="col">{unit.Ld}</th>
+                  <th scope="col">{unit.Sv}+</th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </>
       ))}
-   </div>
-   )
-}
+    </div>
+  );
+};
 export default Unit;
